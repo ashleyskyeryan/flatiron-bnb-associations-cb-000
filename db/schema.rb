d30851bb18060caf97655d8f4b220e1b25a3fcdc
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-13 23:10:06 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113230601) do
+ActiveRecord::Schema.define(version: 20180113230953) do
 
   create_table "cities", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20180113230601) do
   create_table "neighborhoods", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.integer  "city_id"
   end
 
   create_table "reservations", force: :cascade do |t|
