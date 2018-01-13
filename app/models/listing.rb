c50329a51c9b57cc5a80f1d332e41bc23c993929
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-13 22:47:47 +0000

    automatically backed up by learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < ActiveRecord::Base
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :guests through: :reservations
+  has_many :reviews through: :guests
 end
