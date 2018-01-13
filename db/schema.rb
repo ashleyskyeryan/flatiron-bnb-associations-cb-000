3107854d933d7e49c60bcd956d9ae1e3c9bbc8a3
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-13 23:04:57 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113225951) do
+ActiveRecord::Schema.define(version: 20180113230442) do
 
   create_table "cities", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,13 +20,14 @@ ActiveRecord::Schema.define(version: 20180113225951) do
   end
 
   create_table "listings", force: :cascade do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "title"
     t.string   "description"
     t.string   "address"
     t.string   "listing_type"
     t.float    "price"
+    t.integer  "neighborhood_id"
   end
 
   create_table "neighborhoods", force: :cascade do |t|
